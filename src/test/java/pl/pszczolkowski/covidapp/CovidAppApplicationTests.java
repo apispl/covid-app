e70415248ef62677cc5914e0f66a49f474d94494
apispl <krlpl17@gmail.com>
2020-05-29 07:15:25 +0200

    Initial commit

---
+++ src/test/java/pl/pszczolkowski/covidapp/CovidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pszczolkowski.covidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
